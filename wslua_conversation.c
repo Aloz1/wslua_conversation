d24f912bf50e1e4d1c9f3fc8007427cabbf6e2ad
Aloz1 <kno0001@gmail.com>
2022-03-15 23:26:49 +1100

    Fix use-after-free segfault with '__gc' metamethod

--- wslua_conversation.c
+++ wslua_conversation.c
@@ -31,24 +31,20 @@
 #include <lauxlib.h>
 #include <lua.h>
 
-/* FIXME: This is a bit hacky. A segfault occurs when reloading lua scripts in Wireshark, which is
- *        probably due to 'saving' the lua state, then it being reset. Perhaps a __gc metamethod
- *        can be used as a hook to ensure consistency.
+/* This is a bit hacky. Cleanup is handled by using __gc to ensure memory is marked as
+ * unreferenced / NULL if lua state is ever reset, ensuring consistency between Lua land and
+ * wireshark land.
  *
- *        Additionally, I'm not sure if wireshark pinfo's are fully reset on lua script reload (e.g.
- *        fd_visited might always return TRUE when a lua script is reset). If not, then the data
- *        that is registered with wireshark *_add_proto_data functions cannot simply be stored as a
- *        lua registry reference, as it would need to exist independently of Lua state (note, it
- *        could still remain as a lua ref, and only be copied on __gc to improve speed). It looks
- *        like GVariant's (a glib native dynamic typing mechanism) may work well for this, as its
- *        already provided for 'free' with glib, and seems reasonably compareable to lua tables.
+ * This seems to work quite well. No need to serialise/copy values to something more 'C' friendly
+ * (e.g. g_variant fields) as it seems that dissection is fully reset when lua scripts are reloaded
+ * (ctrl+shift+L)
  */
 typedef struct _luareg_data_t {
     lua_State *L;
     int reg_ref;
 } luareg_data_t;
 
-static gboolean luamem_callback(wmem_allocator_t* alloc, wmem_cb_event_t event, void* user_data)
+static gboolean luareg_data_callback(wmem_allocator_t* alloc, wmem_cb_event_t event, void* user_data)
 {
     luareg_data_t *data = (luareg_data_t*)user_data;
 
@@ -57,11 +53,55 @@ static gboolean luamem_callback(wmem_allocator_t* alloc, wmem_cb_event_t event, 
         return TRUE;
     }
 
-    // Remove data from Lua registry
-    luaL_unref(data->L, LUA_REGISTRYINDEX, data->reg_ref);
-    data->reg_ref = LUA_NOREF;
+    if ((data->L != NULL) && (data->reg_ref != LUA_NOREF)) {
+        // Remove data from Lua registry, if lua state still valid
+        luaL_unref(data->L, LUA_REGISTRYINDEX, data->reg_ref);
+        lua_gc(data->L, LUA_GCCOLLECT, 0);
+
+        data->L = NULL;
+        data->reg_ref = LUA_NOREF;
+    }
 
     return FALSE; // Unregister callback once called
+}
+
+static int cleanup_luareg_data(lua_State *L) {
+    int i = lua_upvalueindex(1);
+
+    luaL_checktype(L, i, LUA_TLIGHTUSERDATA);
+    luareg_data_t *data = (luareg_data_t*)lua_topointer(L, i);
+
+    data->L = NULL;
+    data->reg_ref = LUA_NOREF; // No need to unreference. This method is only called for cleanup anyway.
+
+    return 0;
+}
+
+static void create_luareg_table(lua_State *L, luareg_data_t *data) {
+    // Create base table
+    lua_createtable(L, 1, 0);
+
+    // Swap table with passed in data
+    lua_insert(L, -2);
+
+    // Insert data into table
+    lua_rawseti(L, -2, 1);
+
+    // Create metatable
+    lua_createtable(L, 0, 1);
+
+    // Create closure
+    lua_pushlightuserdata(L, data);
+    lua_pushcclosure(L, &cleanup_luareg_data, 1);
+
+    // Set closure as __gc, and pop
+    lua_setfield(L, -2, "__gc");
+
+    // Set metatable
+    lua_setmetatable(L, -2);
+
+    data->L = L;
+    data->reg_ref = luaL_ref(L, LUA_REGISTRYINDEX);
 }
 
 static int set_conv_data(lua_State *L) {
@@ -79,19 +119,21 @@ static int set_conv_data(lua_State *L) {
         // If not, create user data and attach
         conv_data = wmem_new(wmem_file_scope(), luareg_data_t);
 
-        // Pushes stack value onto reference
-        conv_data->reg_ref = luaL_ref(L, LUA_REGISTRYINDEX);
-        conv_data->L = L;
+        conv_data->L = NULL;
+        conv_data->reg_ref = LUA_NOREF;
 
         conversation_add_proto_data(conversation, proto->hfid, conv_data);
-        wmem_register_callback(wmem_file_scope(), luamem_callback, conv_data);
-    } else if (conv_data->L != L) {
-        // Pushes stack value onto reference
-        conv_data->reg_ref = luaL_ref(L, LUA_REGISTRYINDEX);
-        conv_data->L = L;
+        wmem_register_callback(wmem_file_scope(), luareg_data_callback, conv_data);
+    }
+
+    if (conv_data->L != L) {
+        // Not yet setup, lets set it up
+        create_luareg_table(L, conv_data);
     } else {
         // Set data
-        lua_rawseti(conv_data->L, LUA_REGISTRYINDEX, conv_data->reg_ref);
+        lua_rawgeti(conv_data->L, LUA_REGISTRYINDEX, conv_data->reg_ref);
+        lua_insert(L, -2); // Swap table and data
+        lua_rawseti(L, -2, 1);
     }
     
     return 0; // Number of values on lua stack
@@ -112,6 +154,7 @@ static int get_conv_data(lua_State *L) {
         lua_pushnil(L);
     } else {
         lua_rawgeti(conv_data->L, LUA_REGISTRYINDEX, conv_data->reg_ref);
+        lua_rawgeti(L, -1, 1);
     }
 
     return 1; // Number of values on lua stack
@@ -129,20 +172,22 @@ static int set_packet_data(lua_State *L) {
         // If not, create user data and attach
         p_data = wmem_new(wmem_file_scope(), luareg_data_t);
 
-        // Pushes stack value onto reference
-        p_data->reg_ref = luaL_ref(L, LUA_REGISTRYINDEX);
-        p_data->L = L;
+        p_data->L = NULL;
+        p_data->reg_ref = LUA_NOREF;
 
         // Add proto_data
         p_add_proto_data(wmem_file_scope(), pinfo->ws_pinfo, proto->hfid, 0, p_data);
-        wmem_register_callback(wmem_file_scope(), luamem_callback, p_data);
-    } else if (p_data->L != L) {
+        wmem_register_callback(wmem_file_scope(), luareg_data_callback, p_data);
+    }
+
+    if (p_data->L != L) {
         // Pushes stack value onto reference
-        p_data->reg_ref = luaL_ref(L, LUA_REGISTRYINDEX);
-        p_data->L = L;
+        create_luareg_table(L, p_data);
     } else {
         // Set data
-        lua_rawseti(p_data->L, LUA_REGISTRYINDEX, p_data->reg_ref);
+        lua_rawgeti(p_data->L, LUA_REGISTRYINDEX, p_data->reg_ref);
+        lua_insert(L, -2); // Swap table and data
+        lua_rawseti(L, -2, 1);
     }
     
     return 0;
@@ -161,6 +206,7 @@ static int get_packet_data(lua_State *L) {
         lua_pushnil(L);
     } else {
         lua_rawgeti(p_data->L, LUA_REGISTRYINDEX, p_data->reg_ref);
+        lua_rawgeti(L, -1, 1);
     }
 
     return 1; // Number of values on lua stack
